610961b03eec0dc7be7d063c768685b8684d740c
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-01-09 18:21:19 +0530

    add 02-variables

---
+++ 02-variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+func main(){
+	var username string = "sumit"
+	fmt.Println(username)
+	fmt.Printf("Variable is type: %T /n", username)
+	var web = "masterji.co"
+	fmt.Println(web)
+
+}
+
+ 
