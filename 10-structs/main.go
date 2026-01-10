d8b36d3b4192f0ad57e6becadd972b3c0ab2bbec
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-01-10 09:59:35 +0530

    done structs

---
+++ 10-structs/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("This are structs in golang")
+	// no inheritance in go; no super or parent
+	sumit := User{"sumit", "sumit@hotmail.com", true, 23}
+	fmt.Println(sumit)
+	fmt.Printf("sumit details are: %+v\n", sumit)
+	fmt.Printf("Name is %v and email is %v ", sumit.Name, sumit.Email)
+
+}
+
+type User struct {
+	Name     string
+	Email    string
+	Verified bool
+	Age      int
+}
